96cf9c1623fcaa7ee226fe97e0fedf08ff890e24
Huy Tran <tranviquanghuy@gmail.com>
2024-11-26 20:26:12 +0700

    setup layout and header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import "./App.css";
+import Layout from "./layouts/layout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<span>Home Page</span>} />
+        <Route path="/" element={<Layout>Home Page</Layout>} />
 
-        <Route path="/user-profile" element={<span>user-profile</span>} />
+        <Route path="/user-profile" element={<Layout>user-profile</Layout>} />
 
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
