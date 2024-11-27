a05c5ee21384f6d13e2cbe4966c4cddd07339ee6
Huy Tran <tranviquanghuy@gmail.com>
2024-11-27 12:37:28 +0700

    responsive landing page and footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./layouts/layout";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout>Home Page</Layout>} />
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <HomePage />
+            </Layout>
+          }
+        />
 
         <Route path="/user-profile" element={<Layout>user-profile</Layout>} />
 
