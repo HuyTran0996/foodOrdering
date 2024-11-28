a7a428d179cab8e8ca2bc7aa215dcd81fabc75c8
Huy Tran <tranviquanghuy@gmail.com>
2024-11-28 12:52:47 +0700

    auth0 setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,27 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./layouts/layout";
 import HomePage from "./pages/HomePage";
+import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Layout>
-              <HomePage />
-            </Layout>
-          }
-        />
+      <Auth0ProviderWithNavigate>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Layout>
+                <HomePage />
+              </Layout>
+            }
+          />
 
-        <Route path="/user-profile" element={<Layout>user-profile</Layout>} />
+          <Route path="/user-profile" element={<Layout>user-profile</Layout>} />
 
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Auth0ProviderWithNavigate>
     </BrowserRouter>
   );
 }
